1a6deac4e033f1e2e721de6852ae88362f80370f
Boa Mia <boamia@Boas-MacBook-Pro.local>
2023-03-07 14:40:40 +1100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'a15ngrxstore-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'a15-ngrxstore-2023';
+}
