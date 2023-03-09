3adf5c26e514e4753e9f3a62f751f6bbcd992e25
Boa Mia <boamia@Boas-MacBook-Pro.local>
2023-03-09 11:22:31 +1100

    a15m: secure auth

--- src/contracts/interfaces/email-pswd-interface.ts
+++ src/contracts/interfaces/email-pswd-interface.ts
@@ -1,4 +1,4 @@
 export interface EmailPswdInterface {
   email: string;
-  pswd: string;
+  password: string;
 }
---
+++ src/services/external/authen/authen.service.ts
@@ -0,0 +1,41 @@
+import { Logout } from './../../../store/authStore/auth.actions';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+
+import { AppStoreStateInterface } from '../../../contracts/interfaces/app-store-state-interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthenService {
+  private tokenExpirationTimer: number = 0;
+
+  constructor(
+    private httpCli: HttpClient,
+    private router: Router,
+    private store: Store<AppStoreStateInterface>
+  ) {}
+
+  /**
+   * setLogoutTimer
+   */
+  public setLogoutTimer(expirationDuration: number) {
+    this.tokenExpirationTimer = Number(
+      setTimeout(() => {
+        this.store.dispatch(Logout());
+      }, expirationDuration)
+    );
+  }
+
+  /**
+   * clearLogoutTimer
+   */
+  public clearLogoutTimer() {
+    if (this.tokenExpirationTimer) {
+      clearTimeout(this.tokenExpirationTimer);
+    }
+    this.tokenExpirationTimer = 0;
+  }
+}
